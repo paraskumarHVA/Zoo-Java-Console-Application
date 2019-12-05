283f3f76078665832ae6aeec5cd24c975358231d
Paras <p>
2019-12-05 19:01:38 +0100

    Add Animals classes.

---
+++ src/main/java/com/ing/Animals/Pig.java
@@ -0,0 +1,51 @@
+package main.java.com.ing.Animals;
+
+import java.util.Random;
+
+public class Pig extends Animal implements Carnivore, Herbivore {
+    private String helloText;
+    private String eatText;
+    private String trick;
+
+    public Pig(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public void sayHello()
+    {
+        helloText = "splash";
+        System.out.println(helloText);
+    }
+
+    @Override
+    public void eatLeaves()
+    {
+        eatText = "munch munch oink";
+        System.out.println(eatText);
+    }
+
+    @Override
+    public void eatMeat()
+    {
+        eatText = "nomnomnom oink thx";
+        System.out.println(eatText);
+    }
+
+    @Override
+    public void performTrick()
+    {
+        Random random = new Random();
+        int rnd = random.nextInt(2);
+        if(rnd == 0)
+        {
+            trick = "rolls in the mud";
+        }
+        else
+        {
+            trick = "runs in circles";
+        }
+        System.out.println(trick);
+    }
+}
---
+++ src/main/java/com/ing/Animals/Zebra.java
@@ -0,0 +1,25 @@
+package main.java.com.ing.Animals;
+
+public class Zebra extends Animal implements Herbivore {
+    private String helloText;
+    private String eatText;
+
+    public Zebra(String name)
+    {
+        super(name);
+    }
+
+    @Override
+    public void sayHello()
+    {
+        helloText = "zebra zebra";
+        System.out.println(helloText);
+    }
+
+    @Override
+    public void eatLeaves()
+    {
+        eatText = "munch munch zank yee bra";
+        System.out.println(eatText);
+    }
+}
